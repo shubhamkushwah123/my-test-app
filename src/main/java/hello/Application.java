6906aa3353f3aeea3e1576bb2acf855f14859bfe
Shubham Singh Kushwah <shubham@Shubhams-MacBook-Pro.local>
2018-11-18 23:20:44 +0100

    updated again

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import hello.logger.Log;
-
 
 @SpringBootApplication
 @RestController
