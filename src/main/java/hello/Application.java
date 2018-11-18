93bb68bf164362777259eaca8ee93e5bf982b10a
Shubham Singh Kushwah <shubham@Shubhams-MacBook-Pro.local>
2018-11-18 23:17:31 +0100

    updated

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,25 +1,31 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hello.logger.Log;
 
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 	
 	@RequestMapping("/")
     public String home() {
+		log.info("Default / endpoint is called");
+		log.debug("it returns hello from Docker World");
         return "Hello Docker World";
     }
 	
 	
 	public static void main(String[] args) {
-		System.out.println("hello from Docker World");
+		log.info("Main method has been invoked");
 		SpringApplication.run(Application.class,args);
 	}
 
--- src/main/java/hello/MyTestController.java
+++ src/main/java/hello/MyTestController.java
@@ -1,19 +1,26 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyTestController {
 	
+	private static final Logger log = LoggerFactory.getLogger(MyTestController.class);
+	
 	@RequestMapping("/mytest")
-	public void myTestMethod() {
-		System.out.println("I am running here babe");
+	public String myTestMethod() {
+		log.info("hello, its an info");
+		log.debug("hello, its an debug");
+		return "mytest";
 	}
 
 	@RequestMapping("/hello")
 	public String hello() {
-		System.out.println("Hello from test app");
+		//System.out.println("Hello from test app");
+		log.info("hello from test app");
 		return "hello from test app";
 	}
 }
